f8e1fdde3cf481b1df51de6a3ee4f43f3374f012
Rakib Linux <rakibulinux@gmail.com>
2022-11-25 02:02:25 +0600

    modal form added

--- src/Pages/Home/SecondHandProductCategories/BookingModal/BookingModal.js
+++ src/Pages/Home/SecondHandProductCategories/BookingModal/BookingModal.js
@@ -1,6 +1,55 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import toast from "react-hot-toast";
+import { AuthContext } from "../../../../contexts/AuthProvider";
 
-const BookingModal = () => {
+const BookingModal = ({ usedPhone }) => {
+  const [booking, setBooking] = useState(null);
+  const { user } = useContext(AuthContext);
+  const {
+    phoneName,
+    originalPrice,
+    resalePrice,
+    picture,
+    publishedDate,
+    location,
+    yearsOfUse,
+  } = usedPhone;
+  const handleBooking = (e) => {
+    e.preventDefault();
+    const form = e.target;
+
+    const name = form.name.value;
+    const email = form.email.value;
+    const phone = form.phone.value;
+    const booking = {
+      patient: name,
+
+      email,
+      phone,
+      originalPrice: originalPrice,
+      resalePrice: resalePrice,
+    };
+    console.log(booking);
+    // setTreatment(null);
+    fetch("https://doctors-portal-server-nu-two.vercel.app/bookings", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(booking),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.acknowledged) {
+          setBooking(null);
+          toast.success("Booking success");
+          //   refetch();
+        } else {
+          toast.error(data.message);
+        }
+      });
+  };
   return (
     <div className="w-full flex justify-center items-center text-center p-4">
       {/* The button to open modal */}
@@ -20,21 +69,43 @@ const BookingModal = () => {
           >
             X
           </label>
-          <h3 className="font-bold text-lg">
-            Congratulations random Internet user!
-          </h3>
-          <p className="py-4">
-            You've been selected for a chance to get one year of subscription to
-            use Wikipedia for free!
-          </p>
-          <div className="modal-action">
-            <label
-              htmlFor="booking-modal"
-              className="hover:text-gray-100 bg-gradient-to-r from-cyan-500 to-sky-600 w-full px-8 py-3 font-semibold rounded-md bg-gray-900 hover:bg-gray-700 text-gray-100 text-center cursor-pointer"
-            >
-              Book Now
-            </label>
-          </div>
+          <form onSubmit={handleBooking}>
+            <input
+              defaultValue={user?.displayName}
+              type="text"
+              name="name"
+              placeholder="Your name"
+              className="mt-4 input input-bordered w-full"
+              disabled
+            />
+            <p className="mt-4 bordered w-full">
+              Original price: {originalPrice}
+            </p>
+            <p className="mt-4 bordered w-full">Resale price: {resalePrice}</p>
+            <input
+              defaultValue={user?.email}
+              type="email"
+              name="email"
+              placeholder="Your email"
+              className="mt-4 input input-bordered w-full"
+              required
+              disabled
+            />
+            <input
+              type="text"
+              name="phone"
+              placeholder="Your phone"
+              className="mt-4 input input-bordered w-full"
+            />
+            <div className="modal-action">
+              <label
+                htmlFor="booking-modal"
+                className="hover:text-gray-100 bg-gradient-to-r from-cyan-500 to-sky-600 w-full px-8 py-3 font-semibold rounded-md bg-gray-900 hover:bg-gray-700 text-gray-100 text-center cursor-pointer"
+              >
+                Book Now
+              </label>
+            </div>
+          </form>
         </div>
       </div>
     </div>
--- src/Pages/Home/SecondHandProductCategories/ProductsCategories/ProductDetails.js
+++ src/Pages/Home/SecondHandProductCategories/ProductsCategories/ProductDetails.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PrimaryButton from "../../../../components/Button/PrimaryButton";
 import BookingModal from "../BookingModal/BookingModal";
 
 const ProductDetails = ({ usedPhone }) => {
@@ -18,6 +17,10 @@ const ProductDetails = ({ usedPhone }) => {
       <figure>
         <img className="h-96" src={picture} alt={phoneName} />
       </figure>
+      <div className="flex items-center gap-4 my-2">
+        <img className="rounded-full w-10" src={img} alt={name} />
+        <p>{name}</p>
+      </div>
       <div className="card-body">
         <h2 className="card-title">{phoneName}</h2>
         <p>
@@ -36,7 +39,7 @@ const ProductDetails = ({ usedPhone }) => {
           <span className="font-bold">Posted Time:</span> {publishedDate}
         </p>
         <div className="card-actions w-full">
-          <BookingModal />
+          <BookingModal usedPhone={usedPhone} />
         </div>
       </div>
     </div>
